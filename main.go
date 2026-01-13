df2a94a0bd224147f15dd79b1c42f2e271482f8d
Aleksander Gencel <johnnylittleplanet@gmail.com>
2026-01-13 18:39:09 +0100

    feat: move source urls to a separate configuration file

--- main.go
+++ main.go
@@ -10,43 +10,50 @@ import (
 	"sort"
 	"strings"
 	"sync"
+
+	"github.com/goccy/go-yaml"
 )
+
+type Source struct {
+	Name    string `yaml:"name"`
+	RepoURL string `yaml:"repo_url"`
+	DataURL string `yaml:"data_url"`
+	Type    string `yaml:"type"`
+}
+
+type Config struct {
+	Input []Source `yaml:"input"`
+}
 
 var domainRegex = regexp.MustCompile(`^(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])$`)
 
 func main() {
-	urls := []string{
-		"https://raw.githubusercontent.com/disposable-email-domains/disposable-email-domains/main/disposable_email_blocklist.conf",
-		"https://raw.githubusercontent.com/disposable/disposable-email-domains/master/domains_strict.txt",
-		"https://raw.githubusercontent.com/TheDahoom/disposable-email/main/blacklist.txt",
-		"https://raw.githubusercontent.com/eser/sanitizer-svc/main/disposable_email_blocklist.conf",
-		"https://raw.githubusercontent.com/GeroldSetz/emailondeck.com-domains/master/emailondeck.com_domains_from_bdea.cc.txt",
-		"https://raw.githubusercontent.com/groundcat/disposable-email-domain-list/master/domains.txt",
-		"https://raw.githubusercontent.com/jespernissen/disposable-maildomain-list/master/disposable-maildomain-list.txt",
-		"https://raw.githubusercontent.com/kslr/disposable-email-domains/master/list.txt",
-		"https://raw.githubusercontent.com/MattKetmo/EmailChecker/master/res/throwaway_domains.txt",
-		"https://raw.githubusercontent.com/unkn0w/disposable-email-domain-list/main/domains.txt",
-		"https://raw.githubusercontent.com/7c/fakefilter/main/txt/data.txt",
-		"https://raw.githubusercontent.com/wesbos/burner-email-providers/master/emails.txt",
-		"https://raw.githubusercontent.com/FGRibreau/mailchecker/master/list.txt",
-		"https://raw.githubusercontent.com/willwhite/freemail/master/data/free.txt",
-		"https://raw.githubusercontent.com/sublime-security/static-files/master/disposable_email_providers.txt",
+	configFile, err := os.ReadFile("config.yml")
+	if err != nil {
+		fmt.Printf("Error reading config.yml: %v\n", err)
+		return
+	}
+
+	var config Config
+	if err := yaml.Unmarshal(configFile, &config); err != nil {
+		fmt.Printf("Error parsing config.yml: %v\n", err)
+		return
 	}
 
 	var wg sync.WaitGroup
 	domains := make(map[string]struct{})
 	var mu sync.Mutex
 
-	fmt.Println("Downloading domain names from", len(urls), "sources...")
+	fmt.Println("Downloading domain names from", len(config.Input), "sources...")
 
-	for _, url := range urls {
+	for _, source := range config.Input {
 		wg.Add(1)
-		go func(targetUrl string) {
+		go func(src Source) {
 			defer wg.Done()
 
-			resp, err := http.Get(targetUrl)
+			resp, err := http.Get(src.DataURL)
 			if err != nil {
-				fmt.Printf("Failed to download %s: %v\n", url, err)
+				fmt.Printf("Failed to download %s: %v\n", src.Name, err)
 				return
 			}
 			defer resp.Body.Close()
@@ -70,8 +77,8 @@ func main() {
 					localCount++
 				}
 			}
-			fmt.Printf("Downloaded and corrected %d domain names from %s\n", localCount, targetUrl)
-		}(url)
+			fmt.Printf("Downloaded and corrected %d domain names from %s.\n", localCount, src.Name)
+		}(source)
 	}
 
 	wg.Wait()
@@ -108,5 +115,5 @@ func saveToFile(domains map[string]struct{}, filename string) {
 	}
 	writer.Flush()
 
-	fmt.Printf("Successfully saved %d domain names to %s\n", len(sortedDomains), filename)
+	fmt.Printf("Successfully saved %d domain names to %s.\n", len(sortedDomains), filename)
 }
